d87627529e91046abcf48a2a4339463dbab723c1
Ndèye Awa Goumbala Sambe <goumbalasambe@gmail.com>
2024-09-04 19:15:47 +0000

    modif 2

--- reprise1.2/script.js
+++ reprise1.2/script.js
@@ -7,6 +7,7 @@ class JeemaCoder extends React.Component {
       emailInput: "",
       telephoneInput: "",
       coders: [],
+      editIndex: null
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -26,6 +27,17 @@ class JeemaCoder extends React.Component {
       telephoneInput: "",
     });
   }
+
+  handleEdit(index) {
+    const coder = this.state.coders[index]
+    this.setState({
+      prenomInput: coder.prenom,
+      nomInput: coder.nom,
+      emailInput: coder.email,
+      telephoneInput: coder.telephone,
+    })
+  }
+
   render() {
     return (
       <div className="py-4">
@@ -34,7 +46,7 @@ class JeemaCoder extends React.Component {
           <div style={{ maxWidth: 600, margin: "auto" }}>
             <div className="row">
               <div className="col-6 p-1">
-                <label className="form-label">Prenom {this.state.prenomInput}</label>
+                <label className="form-label">Prenom</label>
                 <input
                   type="text"
                   value={this.state.prenomInput}
@@ -99,16 +111,26 @@ class JeemaCoder extends React.Component {
                 <th scope="col">Nom</th>
                 <th scope="col">Email</th>
                 <th scope="col">Telephone</th>
+                <th scope="col">Actions</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.coders.map((coder) => {
+              {this.state.coders.map((coder, index) => {
                 return (
                   <tr>
                     <td>{coder.prenom}</td>
                     <td>{coder.nom}</td>
                     <td>{coder.email}</td>
                     <td>{coder.telephone}</td>
+                    <td>
+                      <button className="btn btn-outline-warning"
+                        onClick= {() => this.handleEdit(index)}
+                      >
+                        Modifier
+                      </button>
+
+                    </td>
+
                   </tr>
                 );
               })}
